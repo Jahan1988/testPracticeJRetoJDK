145770b9edcee8b8e754d2b3c15d7449638cbee2
Your Name <you@example.com>
2019-12-21 18:05:23 -0500

    practice

---
+++ src/main/java/com/practiceJREtoJDK/com/practiceJREtoJDK/App.java
@@ -0,0 +1,13 @@
+package com.practiceJREtoJDK.com.practiceJREtoJDK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
